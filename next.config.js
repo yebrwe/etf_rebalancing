d4ef86a7e2ff4836b3f2646c1cdf4ce624d567ab
이훈경 <yebrwe@naver.com>
2024-11-07 17:55:29 +0900

    Fix: 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+        {
+            // API 라우트에 대한 CORS 설정
+            source: '/api/:path*',
+            headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST' },
+            { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+            ],
+        },
+        ]
+    },
   reactStrictMode: true,
   env: {
     KOREA_INVESTMENT_APP_KEY: process.env.KOREA_INVESTMENT_APP_KEY,
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // API 라우트에 대한 CORS 설정
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
+  },
   /* config options here */
+  env: {
+    KOREA_INVESTMENT_APP_KEY: process.env.KOREA_INVESTMENT_APP_KEY,
+    KOREA_INVESTMENT_APP_SECRET: process.env.KOREA_INVESTMENT_APP_SECRET,
+  },
 };
 
 export default nextConfig;
